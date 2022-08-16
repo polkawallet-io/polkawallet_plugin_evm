ae170d418532e792e5fed69815b5249e7aafda1a
dingzuhua <dingzuhua@qq.com>
2022-08-16 17:15:38 +0800

    add networkType

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -4,11 +4,15 @@ const network_node_list = {
       'name': 'acala (via RadiumBlock)',
       'ss58': 2,
       'endpoint': 'https://karura-dev.aca-dev.network/eth/http',
+      'chainId': '',
+      'networkType': 'ethereum',
     },
     {
       'name': 'acala (via RadiumBlock)',
       'ss58': 2,
       'endpoint': 'https://karura-dev.aca-dev.network/eth/http',
+      'chainId': '',
+      'networkType': 'ethereum',
     },
   ],
   'karura': [
@@ -16,11 +20,15 @@ const network_node_list = {
       'name': 'karura (via RadiumBlock)',
       'ss58': 2,
       'endpoint': 'https://karura-dev.aca-dev.network/eth/http',
+      'chainId': '',
+      'networkType': 'ethereum',
     },
     {
       'name': 'karura (via RadiumBlock)',
       'ss58': 2,
       'endpoint': 'https://karura-dev.aca-dev.network/eth/http',
+      'chainId': '',
+      'networkType': 'ethereum',
     },
   ]
 };
